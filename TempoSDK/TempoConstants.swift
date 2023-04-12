76c3db3deed19a18c3d3ab2f9ab39e66ef2e49d3
omnibaker <37693234+omnibaker@users.noreply.github.com>
2023-04-12 14:56:41 +1000

    (feature) backups metrics, podspec ~> 0.2.18

--- TempoSDK/TempoConstants.swift
+++ TempoSDK/TempoConstants.swift
@@ -4,5 +4,5 @@ struct TempoConstants {
     static let METRIC_SERVER_URL = "https://metric-api.tempoplatform.com/metrics" // DEV: "https://dev-metric-api.tempoplatform.com/metrics"
     static let METRIC_BACKUOP_FOLDER = "metricJsons"
     static let METRIC_BACKUP_SUFFIX = "_mtarr.tempo"
-    static let IS_DEBUGGING = true
+    static let IS_DEBUGGING = false
 }
