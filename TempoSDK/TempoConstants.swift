2cc1cd85f111be57405c3e144287bd95707a7070
omnibaker <37693234+omnibaker@users.noreply.github.com>
2023-04-27 10:03:35 +1000

    Tested and ready for production, podspec ~> 1.0.0

--- TempoSDK/TempoConstants.swift
+++ TempoSDK/TempoConstants.swift
@@ -1,13 +1,12 @@
 // General constants in use throughout app
 
 struct TempoConstants {
-    static let METRIC_SERVER_URL = "https://dev-metric-api.tempoplatform.com/metrics" // DEV: "https://dev-metric-api.tempoplatform.com/metrics"
+    static let METRIC_SERVER_URL = "https://metric-api.tempoplatform.com/metrics" // DEV: "https://dev-metric-api.tempoplatform.com/metrics"
     static let METRIC_BACKUP_FOLDER = "metricJsons"
     static let METRIC_BACKUP_APPEND = ".tempo"
     static let IS_DEBUGGING = false
-    static let SDK_VERSIONS = "0.0.1"
+    static let SDK_VERSIONS = "1.0.0"
     static let METRIC_TIME_HEADER = "X-Timestamp"
     static let MAX_BACKUPS: Int = 100
     static let EXPIRY_DAYS: Int = 7
-    
 }
