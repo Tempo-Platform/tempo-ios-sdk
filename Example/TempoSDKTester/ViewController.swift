5b57ec232366534707488686eb8753e71ab55510
omnibaker <37693234+omnibaker@users.noreply.github.com>
2023-06-13 13:23:26 +1000

    ready for testing

--- Example/TempoSDKTester/ViewController.swift
+++ Example/TempoSDKTester/ViewController.swift
@@ -5,7 +5,7 @@ class ViewController: UIViewController, TempoInterstitialListener {
 
     
     
-    public static let TEST_APP_ID: String = "1"; // 1 for DEV, 8 for PROD
+    public static let TEST_APP_ID: String = "8"; // 1 for DEV, 8 for PROD
 
     var interstitialReady:Bool = false
     var interstitial:TempoInterstitial? = nil
@@ -140,7 +140,7 @@ class ViewController: UIViewController, TempoInterstitialListener {
     
     func onGetAdapterType() -> String? {
         print("\(getType(isInterstitial: isInterstitial)) :: adapter type requested")
-        return nil;
+        return "APPLOVIN";
     }
     
     func getType(isInterstitial: Bool) -> String
