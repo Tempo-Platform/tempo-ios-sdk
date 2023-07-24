dbc9645382751f203a81744a4ba8c70734592810
omnibaker <37693234+omnibaker@users.noreply.github.com>
2023-07-24 18:12:21 +1000

    Tester changes

--- TempoSDK/TempoTesting.swift
+++ TempoSDK/TempoTesting.swift
@@ -9,41 +9,41 @@ import Foundation
 
 public class TempoTesting {
     
-    private static var instance: TempoTesting?
+    public static var instance: TempoTesting?
     
-    init() {
+    public init() {
         TempoTesting.instance = self
     }
     
-    func toggleTesting() -> Void {
+    public func toggleTesting() -> Void {
         Constants.IS_TESTING = !Constants.IS_TESTING
     }
     
-    func updateEnvironment(isProd: Bool) -> Void {
+    public func updateEnvironment(isProd: Bool) -> Void {
         Constants.IS_PROD = isProd
     }
     
-    func activateUseOfDeployVersion(activate: Bool) -> Void {
+    public func activateUseOfDeployVersion(activate: Bool) -> Void {
         Constants.isTestingDeployVersion = activate
     }
     
-    func updateDeployVersion(newVersion: String) -> Void {
+    public func updateDeployVersion(newVersion: String) -> Void {
         Constants.currentDeployVersion = newVersion
     }
     
-    func activateCustomCampaignIdsForInterstitialAds(activate: Bool) -> Void {
+    public func activateCustomCampaignIdsForInterstitialAds(activate: Bool) -> Void {
         Constants.isTestingCustomCampaignIdsForInterstitialAds = activate
     }
     
-    func activateCustomCampaignIdsForRewardedAds(activate: Bool) -> Void {
+    public func activateCustomCampaignIdsForRewardedAds(activate: Bool) -> Void {
         Constants.isTestingCustomCampaignIdsForRewardedAds = activate
     }
     
-    func updateCampaignForInterstitialAd(newVersion: String) -> Void {
+    public func updateCampaignForInterstitialAd(newVersion: String) -> Void {
         Constants.customCampaignIdForInterstitial = newVersion
     }
     
-    func updateCampaignForRewardedAd(newVersion: String) -> Void {
+    public func updateCampaignForRewardedAd(newVersion: String) -> Void {
         Constants.customCampaignIdForRewarded = newVersion
     }
 }
