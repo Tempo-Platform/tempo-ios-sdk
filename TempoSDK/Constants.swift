24fb94f40a0b03c57901e3742f2dcd09428f1b90
omnibaker <37693234+omnibaker@users.noreply.github.com>
2023-11-02 09:56:54 +1100

    turn of debug...

--- TempoSDK/Constants.swift
+++ TempoSDK/Constants.swift
@@ -87,6 +87,6 @@ public struct Constants {
     
     // Testable variables
     public static var isProd = true
-    public static var isTesting = true
+    public static var isTesting = false
     
 }
