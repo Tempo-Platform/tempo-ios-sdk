076889983fb5af44f72daa398ce5ea38b410512f
omnibaker <37693234+omnibaker@users.noreply.github.com>
2024-04-18 15:27:00 +1000

    rc.20 19 not working?

--- TempoSDK/Constants.swift
+++ TempoSDK/Constants.swift
@@ -2,7 +2,7 @@
 
 public struct Constants {
     
-    public static let SDK_VERSIONS = "1.4.1-rc.19"
+    public static let SDK_VERSIONS = "1.4.1-rc.20"
     static let NO_FILL = "NO_FILL"
     static let OK = "OK"
     static let UNDEF = "UNDEFINED"
@@ -88,5 +88,6 @@ public struct Constants {
     // Testable variables
     public static var isProd = true
     public static var isTesting = false
+    public static var isInit = true
     
 }
