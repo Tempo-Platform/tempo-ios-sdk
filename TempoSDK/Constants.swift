f40003b795667f9943350302b4293552628ae827
omnibaker <37693234+omnibaker@users.noreply.github.com>
2024-06-28 14:11:08 +1000

    1.6.2-rc.0

--- TempoSDK/Constants.swift
+++ TempoSDK/Constants.swift
@@ -2,7 +2,7 @@
 
 public struct Constants {
     
-    public static let SDK_VERSIONS = "1.6.1-rc.2"
+    public static let SDK_VERSIONS = "1.6.2-rc.0"
     static let NO_FILL = "NO_FILL"
     static let OK = "OK"
     static let UNDEF = "UNDEFINED"
